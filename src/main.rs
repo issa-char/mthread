d6af67b188dde681af20762510a4d4760fa63bc6
issa-char <128964601+issa-char@users.noreply.github.com>
2024-06-15 12:45:56 +0300

    simple single threaded

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::net::TcpListener;
+
+fn main() {
+
+    let listener = TcpListener::bind("127.0.0.1:8081").unwrap();
+    
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        println!("connection established")
+    }
+
+    
+
+}
